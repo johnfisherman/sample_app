b8ab73ab7e554258244fc162869f57c09439beea
fred <john.fisherman@gmail.com>
2012-02-22 11:43:40 +0000

    Remove default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
 
-  get "static_pages/help"
+  # automatically creates named routes for use in the controllers and views
+  match '/help',    to: 'static_pages#help'
+  # matches ’/about’ and routes it to the about action in the StaticPages controller.
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
   
-  get "static_pages/about"
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
