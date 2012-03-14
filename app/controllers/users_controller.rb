ea60258b03531e6638364625525760f818ccb14e
fred <john.fisherman@gmail.com>
2012-03-14 18:01:12 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,20 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
+  end
+  
+  def create
+    # submitting the form results in a user hash with attributes 
+    # corresponding to the submitted values, where the keys come 
+    # from the name attributes of the input tags (HTML side)
+    @user = User.new(params[:user])
+    if @user.save
+      # we can omit the user_path in the redirect, writing simply redirect_to @user to redirect to the user show page
+      flash[:success] = "User created successfully! You are now one of us!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
