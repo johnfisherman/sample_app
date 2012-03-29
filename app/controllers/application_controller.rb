cb2a6561661c9094fe7bb52356e9ca8b59324e9e
fred <john.fisherman@gmail.com>
2012-03-29 23:31:14 +0100

    + commented, notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   # We could make an entirely new module for authentication, but the Sessions controller 
   # already comes equipped with a module, namely, SessionsHelper. Moreover, such helpers are automatically included in Rails views, 
-  # so all we need to do to use the Sessions helper functions in controllers is to include the module into the Application controlle
+  # so all we need to do to use the Sessions helper functions in controllers is to include the module into the Application controller
   include SessionsHelper
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,18 +3,24 @@ module SessionsHelper
     def sign_in(user)
       # add current user's remember token to cookie, that will last 20 yrs (permanent). So we can retrieve the user on subsequent page views.
       cookies.permanent[:remember_token] = user.remember_token
+      # calls method current_user= . The purpose of this line is to create current_user, accessible in both controllers and views.
+      # which will allow constructions such as <%= current_user.name %>
       current_user = user
     end
     
     def signed_in?
+      # ¿ calls current_user getter, that in turn calls user_from_remember_token (if @current_user == nil), that checks for this user's remember_token cookie ?
       !current_user.nil?
     end
     
     # assignment definition
     def current_user=(user)
+      # The one-line method body just sets an instance variable @current_user, effectively storing the user for later use.
       @current_user = user
     end
     
+    # getter of instance variable, @current_user
+    # guarantees states (from page to page user is seen as signed in, because token matches id)
     def current_user
       # calls the user_from_remember_token method the first time current_user is called, but on subsequent invocations 
       # returns @current_user without calling user_from_remember_token. This is only useful if current_user is used more than once for a single user request; 
