83fcde12512da878ec950bafb0de988a43e72a26
fred <fred@hush.(none)>
2012-04-14 16:26:41 +0100

    Add user following. The End.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,17 @@ class User < ActiveRecord::Base
     attr_accessible :name, :email, :password, :password_confirmation
     has_secure_password
     has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+    # user.followeds is rather awkward; far more natural is to use “followed users” as a plural of “followed”, and write instead user.followed_users for the array of followed users
+    # Rails allows us to override the default
+    has_many :followed_users, through: :relationships, source: :followed
+    
+    # ReverseRelationship class doesn't exist
+    has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+    # source optional
+    has_many :followers, through: :reverse_relationships, source: :follower
     before_save :create_remember_token
     
     # presence always beats presents. And uses blank? method, too. So, :name = "   " is invalid.
@@ -23,13 +34,27 @@ class User < ActiveRecord::Base
     validates :password, length: { minimum: 6 }
     
     def feed
+      Micropost.from_users_followed_by(self)
       # This is preliminary. See "Following users" for the full implementation.
-      Micropost.where("user_id = ?", id)
+      # Micropost.where("user_id = ?", id)
       # or, simply
       # microposts
     end
     
+    def following?(other_user)
+      # equivalent to self.relationships.find_by_followed_id(other_user.id)
+      relationships.find_by_followed_id(other_user.id)
+    end
+  
+    def follow!(other_user)
+      relationships.create!(followed_id: other_user.id)
+    end
     
+    def unfollow!(other_user)
+      relationships.find_by_followed_id(other_user.id).destroy
+    end    
+    
+      
     private
       # attribute remember token on user sign up
       def create_remember_token
